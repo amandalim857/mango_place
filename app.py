d30904b59c7028ca741d98320f71aadcf8ad0d08
amanda <amandalim857@gmail.com>
2023-02-23 00:42:04 +0800

    Create initial dummy endpoints for backend and modify run-dev

--- app.py
+++ app.py
@@ -1,8 +1,45 @@
 import flask
-from flask import Flask
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	return flask.render_template("index.html")
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+
+@app.route("/login", methods=['POST','GET'])
+def login():
+	error = None
+	if request.method == 'POST':
+		username = request.form["username"]
+		password = request.form["password"]
+		if valid_login(username, password):
+			return login_user(username)
+		else:
+			error = 'Invalid username/password'
+	return redirect('login.html', error=error)
+
+
+
+@app.route("/canvas")
+def get_canvas():
+	# retrieve entire canvas
+	pass
+
+
+@app.route("/canvas/<int:pixel_id>")
+def update_pixel(pixel_id, rgb):
+	# update pixel to new rgb value
+    change_pixel(pixel_id, rgb)
+    
+
+@app.route("/<int: user_id>/countdown")
+def get_countdown(user_id):
+	# get countdown left for user
+    retrieve_countdown(user_id)
+
