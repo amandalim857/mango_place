fc78605552e4d6f37b68d82cd2e082ad2ab43426
jadenPete <jadenpete@github.com>
2023-02-28 14:55:16 -0500

    Designed the log in and sign up forms

--- static/ts/index/app.component.ts
+++ static/ts/index/app.component.ts
@@ -1,8 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
+
+enum AppModal {
+	None,
+	Landing,
+	LogIn,
+	SignUp
+}
 
 @Component({
 	selector: "app",
-	styleUrls: ["./app.component.scss"],
 	templateUrl: "./app.component.html"
 })
-export class AppComponent {}
+export class AppComponent {
+	public AppModal = AppModal;
+
+	public shownModal: AppModal = AppModal.Landing;
+
+	@ViewChild("password_input")
+	private readonly passwordElement: ElementRef<HTMLInputElement>;
+
+	@ViewChild("password_confirm_input")
+	private readonly passwordConfirmElement: ElementRef<HTMLInputElement>;
+
+	public validatePasswordConfirmation(): void {
+		if (
+			this.passwordElement.nativeElement.value ==
+			this.passwordConfirmElement.nativeElement.value
+		) {
+			this.passwordConfirmElement.nativeElement.setCustomValidity("");
+		} else {
+			console.log("hey");
+			this.passwordConfirmElement.nativeElement.setCustomValidity("Passwords don't match.");
+		}
+	}
+}
