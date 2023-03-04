bd865b5893e0aafd271f8f3c8d4451c3a8390f27
jadenPete <jadenpete@github.com>
2023-03-04 18:43:50 -0500

    Implemented the board control button bar

--- static/ts/index/app.module.ts
+++ static/ts/index/app.module.ts
@@ -2,12 +2,12 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BoardModule } from "@mangoplace/board/board.module";
-import { ModalModule } from "@mangoplace/modal/modal.module";
+import { BootstrapModule } from "@mangoplace/bootstrap/bootstrap.module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
 	bootstrap: [AppComponent],
 	declarations: [AppComponent],
-	imports: [BoardModule, BrowserModule, CommonModule, ModalModule]
+	imports: [BoardModule, BrowserModule, CommonModule, BootstrapModule]
 })
 export class AppModule {}
