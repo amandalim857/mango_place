d416c088a15997200859f1e0422591e02d31521d
amanda <amandalim857@gmail.com>
2023-03-05 22:23:07 +0800

    Add suggestions from PR

--- db.py
+++ db.py
@@ -1,50 +1,44 @@
 import sqlite3
 import bcrypt
 
-def create_connection():
-    conn = sqlite3.connect("schema.db")
-    return conn
+class Database():
+
+    def __init__(self):
+        self.conn = sqlite3.connect("schema.db")
+        self.cur = self.conn.cursor()
 
 
-def create_users_table(conn):
-    cur = conn.cursor()
-    cur.execute("""
-    DROP TABLE IF EXISTS users;
-    CREATE TABLE users(
-        id INTEGER PRIMARY KEY autoincrement,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
-    );""")
-    conn.commit()
+    def create_users_table(self):
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY autoincrement,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
+        );""")
+        self.conn.commit()
 
 
-def add_user(conn, username, password):
-    cur = conn.cursor()
-    userbytes = password.encode("utf-8")
-    salt = bcrypt.gensalt()
-    hash = bcrypt.hashpw(userbytes, salt)
-    cur.execute("INSERT INTO users(username, password) values (?, ?)", (username, hash))
-    conn.commit()
+    def add_user(self, username, password):
+        userbytes = password.encode("utf-8")
+        salt = bcrypt.gensalt()
+        hash = bcrypt.hashpw(userbytes, salt)
+        self.cur.execute("INSERT INTO users(username, password) values (?, ?)", (username, hash))
+        self.conn.commit()
 
 
-def delete_user(conn, username):
-    cur = conn.cursor()
-    cur.execute("DELETE FROM users WHERE username == ?)", (username))
-    conn.commit()
+    def delete_user(self, username):
+        self.cur.execute("DELETE FROM users WHERE username == ?)", (username))
+        self.conn.commit()
 
-# Login validation functions
-def valid_username(username):
-    conn = create_connection()
-    cur = conn.cursor()
-    username_exists = cur.execute("SELECT EXISTS(SELECT 1 FROM users WHERE username == ?)",(username))
-    conn.commit()
-    return username_exists
+    # Login validation functions
+    def valid_username(self, username):
+        username_exists = self.cur.execute("SELECT EXISTS(SELECT 1 FROM users WHERE username == ?)",(username))
+        self.conn.commit()
+        return username_exists
 
-def valid_login(username, password):
-    conn = create_connection()
-    cur = conn.cursor()
-    userbytes = password.encode("utf-8")
-    info = cur.execute("SELECT password FROM users WHERE username == ?)",(username))
-    conn.commit()
-    result = bcrypt.checkpw(userbytes, info[0])
-    return result
+    def valid_login(self, username, password):
+        userbytes = password.encode("utf-8")
+        info = self.cur.execute("SELECT password FROM users WHERE username == ?)",(username))
+        self.conn.commit()
+        result = bcrypt.checkpw(userbytes, info[0])
+        return result
