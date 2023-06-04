1c783f425d753bb667b80e0793d864efaa8d860f
amanda <amandalim857@gmail.com>
2023-06-04 13:17:43 +0800

    Fix signup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template, session, Response, flash
+from flask import Flask, request, redirect, url_for, render_template, session, Response, make_response
 from config import SECRET_KEY
 from db import *
 
@@ -10,19 +10,27 @@ def index():
 	if "username" in session:
 		return render_template('canvas.html')
 	else:
-		redirect(url_for('login'))
+		redirect(url_for("login"))
 
 # Login/ Logout
-@app.route('/logout')
-def logout():
-	session.pop('username', None)
-	return redirect(url_for("login"))
+@app.route("/signup", methods=['POST','GET'])
+def signup():
+	users = UserTable()
+	if request.method == 'POST':
+		username = request.form["username"]
+		password = request.form["password"]
+		if users.valid_username(username):
+			return redirect(url_for("index"), error="account_exists")
+		users.add_user(username, password)
+		return redirect(url_for("index"))
 
 @app.route("/login", methods=['POST','GET'])
 def login():
 	error = None
 	users = UserTable()
 	if request.method == 'POST':
+		if username not in request or password not in request:
+			flash("Username or password missing") # TODO
 		username = request.form["username"]
 		password = request.form["password"]
 		if users.valid_username(username):
@@ -33,6 +41,11 @@ def login():
 		else:
 			error = 'Username does not exist'
 		return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('username', None)
+	return redirect(url_for("login"))
 
 # Canvas
 @app.route("/canvas", methods=['GET'])
@@ -48,8 +61,7 @@ def get_canvas():
 @app.route("/canvas/<int:row>/<int:col>", methods=['PUT'])
 def place_pixel(row, col):
 	hexcolor = request.args.get('hexcolor')
-	# username = session["username"]
-	username = "megan"
+	username = session["username"]
 	red, green, blue = bytes.fromhex(hexcolor)
 	rgb = [red, green, blue]
 	canvas, pixel_table, countdown_table = CanvasTable(), PixelTable(), CountdownTable()
@@ -60,11 +72,12 @@ def place_pixel(row, col):
 		canvas.update_canvas(row, col, rgb)
 		pixel_table.upsert_pixel_data(row, col, username, rgb, timestamp)
 		countdown_table.upsert_user_timestamp(username, timestamp)
-		flash(f'Inserted {row}, {col}')
 	else:
-		min_left = (300 - time_waited) // 60
-		flash(f'You have to wait {min_left} min more before placing your next tile!')
-
+		seconds_left = (300 - time_waited)
+		response = make_response(f"You have to wait {seconds_left} seconds more before placing your next tile!", 429)
+		response.mimetype = "text/plain"
+		response.headers["Retry-After"] = seconds_left
+		return response
 
 users, canvas, pixel_table, countdown_table = UserTable(), CanvasTable(), PixelTable(), CountdownTable()
 users.create_users_table()
